cc34a414f6c6164c36d3ead9d909bc52aadb0659
eberjoe <eberjoe@gmail.com>
2020-01-15 17:29:07 -0300

    Add search by tech

---
+++ backend/src/controllers/SearchController.js
@@ -0,0 +1,17 @@
+const Dev = require('../models/Dev');
+const parseStringAsArray = require('../utils/parseStringAsArray');
+
+module.exports = {
+    async index(request, response) {
+        // buscar todos os devs num raio de 10km
+        // filtrar por tecnologia
+        const { latitude, longitude, techs } = request.query;
+        const techsArray = parseStringAsArray(techs);
+        const devs = await Dev.find({
+            techs: {
+                $in: techsArray,
+            }
+        });
+        return response.json({ devs });
+    }
+}
