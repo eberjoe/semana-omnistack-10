b32882e800a0abf4461bc7bf9adbe7eee760dc7e
eberjoe <eberjoe@gmail.com>
2020-01-15 15:28:30 -0300

    Add model for Devs and POST method

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,7 +2,7 @@ const { Router } = require('express');
 
 const routes = Router();
 
-routes.post('/users', (request, response) => {
+routes.post('/devs', (request, response) => {
     console.log(request.body);
     return response.json({ message: 'Hello Man 2' });
 });
